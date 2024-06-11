8a3156ab2c137063ac04b67e76f20db95567df2b
iankuria668 <iankuria668@gmail.com>
2024-06-11 08:44:50 +0300

    improved error handling

--- models/article.py
+++ models/article.py
@@ -1,36 +1,38 @@
 from models import get_db_connection
+
 class Article:
     def __init__(self, id=None, title=None, content=None, author_id=None, magazine_id=None):
         if id:
-            self.id = id
-            self.title = title
-            self.content = content
-            self.author_id = author_id
-            self.magazine_id = magazine_id
+            self._id = id
+            details = self.fetch_details_by_id(id)
+            self._title = details['title']
+            self._content = details['content']
+            self._author_id = details['author_id']
+            self._magazine_id = details['magazine_id']
+        elif title and content and author_id and magazine_id:
+            self._id = self._insert_article(title, content, author_id, magazine_id)
+            self._title = title
+            self._content = content
+            self._author_id = author_id
+            self._magazine_id = magazine_id
         else:
-            self.id = id
-            self.title = title
-            self.content = content
-            self.author_id = author_id
-            self.magazine_id = magazine_id
+            raise ValueError('Either id or (title, content, author_id, magazine_id) must be provided.')
 
     def _insert_article(self, title, content, author_id, magazine_id):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-
-        cursor.execute('INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?,?,?,?)', (title, content, author_id, magazine_id))
-        conn.commit()
-        article_id = cursor.lastrowid
-        conn.close()
-        return article_id
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?,?,?,?)', (title, content, author_id, magazine_id))
+            conn.commit()
+            return cursor.lastrowid
     
     def fetch_details_by_id(self, id):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM articles WHERE id = ?', (id,))
-        article = cursor.fetchone()
-        conn.close()
-        return article
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM articles WHERE id = ?', (id,))
+            article = cursor.fetchone()
+            if article is None:
+                raise ValueError(f'Article with id {id} does not exist.')
+            return article
     
     @property
     def id(self):
@@ -40,9 +42,23 @@ class Article:
     def title(self):
         return self._title
     
+    @title.setter
+    def title(self, title):
+        if 2 <= len(title) <= 200:
+            self._title = title
+        else:
+            raise ValueError('Title must be between 2 and 200 characters')
+
     @property
     def content(self):
         return self._content
+    
+    @content.setter
+    def content(self, content):
+        if content:
+            self._content = content
+        else:
+            raise ValueError('Content cannot be empty')
     
     @property
     def author_id(self):
