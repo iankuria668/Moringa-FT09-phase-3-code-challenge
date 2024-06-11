967794966c092154088127f9eb439bd4aa5cd3e7
iankuria668 <iankuria668@gmail.com>
2024-06-11 08:42:38 +0300

    Improved error handling

--- models/author.py
+++ models/author.py
@@ -3,30 +3,30 @@ from models import get_db_connection
 class Author:
     def __init__(self, id=None, name=None):
         if id:
-            self.id = id
-            self.name = self._fetch_name_by_id(id)
+            self._id = id
+            self._name = self._fetch_name_by_id(id)
+        elif name:
+            self._id = self._insert_author(name)
+            self._name = name
         else:
-            self.id = id
-            self.name = name._insert_author(name)
+            raise ValueError('Either id or name must be provided.')
 
     def _insert_author(self, name):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-
-        cursor.execute('INSERT INTO authors (name) VALUES (?)', (name,))
-        conn.commit()
-        author_id = cursor.lastrowid
-        conn.close()
-        return author_id
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO authors (name) VALUES (?)', (name,))
+            conn.commit()
+            return cursor.lastrowid
 
     def _fetch_name_by_id(self, id):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT name FROM authors WHERE id = ?', (id,))
-        name = cursor.fetchone()[0]
-        conn.close()
-        return name
-    
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT name FROM authors WHERE id = ?', (id,))
+            result = cursor.fetchone()
+            if result is None:
+                raise ValueError(f'Author with id {id} does not exist.')
+            return result[0]
+
     @property
     def id(self):
         return self._id
@@ -35,23 +35,28 @@ class Author:
     def name(self):
         return self._name
     
+    @name.setter
+    def name(self, name):
+        if 2 <= len(name) <= 100:
+            self._name = name
+        else:
+            raise ValueError('Name must be between 2 and 100 characters')
+
     def articles(self):
         from models import Article
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM articles WHERE author_id = ?', (self.id,))
-        articles = cursor.fetchall()
-        conn.close()
-        return articles
-    
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM articles WHERE author_id = ?', (self.id,))
+            articles = cursor.fetchall()
+            return [Article(*article) for article in articles]
+
     def magazines(self):
         from models import Magazine
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM magazines WHERE id IN (SELECT magazine_id FROM articles WHERE author_id = ?)', (self.id,))
-        magazines = cursor.fetchall()
-        conn.close()
-        return magazines
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM magazines WHERE id IN (SELECT magazine_id FROM articles WHERE author_id = ?)', (self.id,))
+            magazines = cursor.fetchall()
+            return [Magazine(*magazine) for magazine in magazines]
 
     def __repr__(self):
         return f'<Author {self.name}>'
